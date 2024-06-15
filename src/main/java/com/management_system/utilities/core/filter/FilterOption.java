5ff48f582641fef4cfcee6aaab1cf675e6f85194
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-06-16 01:30:54 +0700

    update db filter

--- src/main/java/com/management_system/utilities/core/filter/FilterOption.java
+++ src/main/java/com/management_system/utilities/core/filter/FilterOption.java
@@ -3,8 +3,12 @@ package com.management_system.utilities.core.filter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class FilterOption {
     String type;
+
+    public Map<String, Object> toMap() {return null;}
 }
--- src/main/java/com/management_system/utilities/utils/DbUtils.java
+++ src/main/java/com/management_system/utilities/utils/DbUtils.java
@@ -1,15 +1,20 @@
 package com.management_system.utilities.utils;
 
+import com.management_system.utilities.core.filter.FilterOption;
+import com.management_system.utilities.entities.Pagination;
 import com.management_system.utilities.entities.exceptions.DataNotFoundException;
 import com.mongodb.client.result.UpdateResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -34,5 +39,29 @@ public class DbUtils {
         }
 
         mongoTemplate.updateFirst(query, update, recordClass);
+    }
+
+
+    public <T extends FilterOption, U> List<U> filterData(T filterOptions, Pagination pagination, Class<U> tartgetClass) {
+        Criteria criteria = new Criteria();
+        Map<String, Object> optionMap = filterOptions.toMap();
+        for(String key: optionMap.keySet()) {
+            Object value = optionMap.get(key);
+
+            if(value != null) {
+                if(key.contains("name") && !value.toString().isBlank()) {
+                    criteria.and(key).regex(".*" + value + ".*", "i");
+                }
+                else {
+                    criteria.and(key).is(value);
+                }
+            }
+        }
+
+        Query query = new Query()
+                .addCriteria(criteria)
+                .with(PageRequest.of(pagination.getPage() - 1, pagination.getSize()));
+
+        return mongoTemplate.find(query, tartgetClass);
     }
 }
