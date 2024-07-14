0da3332b1df1530f3170f6038bac2c843bdbd356
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-07-15 00:45:50 +0700

    update

--- src/main/java/com/management_system/utilities/UtilitiesApplication.java
+++ src/main/java/com/management_system/utilities/UtilitiesApplication.java
@@ -1,6 +1,7 @@
 package com.management_system.utilities;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import com.management_system.utilities.utils.CredentialsUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -10,6 +11,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 		scanBasePackages = {"com.management_system.utilities"}
 )
 public class UtilitiesApplication {
+	@Autowired
+	CredentialsUtils credentialsUtils;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UtilitiesApplication.class, args);
