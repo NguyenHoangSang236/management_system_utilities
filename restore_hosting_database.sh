99906ba9886c5cb061b78d09af50fed87551e6df
NguyenHoangSang236 <nguyenhoangsang236@gmail.com>
2024-07-15 13:16:41 +0700

    update restore db script on hosting

--- restore_hosting_database.sh
+++ restore_hosting_database.sh
@@ -2,5 +2,5 @@
 docker cp ./management_system_utilities/database 1cf22f744516:/data/db
 
 # chạy câu lệnh để restore database
-docker exec -it mongodb-container mongorestore -d management_system /data/db/database/management_system
+docker exec -it mongodb-container mongorestore -u root -p 123456 --authenticationDatabase admin -d management_system /data/db/database/management_system
 
