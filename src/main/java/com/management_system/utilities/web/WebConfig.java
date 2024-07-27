3d6a297db0cf743729a7e24011c56282b1dcfbf9
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-07-28 00:31:46 +0700

    update

--- src/main/java/com/management_system/utilities/web/WebConfig.java
+++ src/main/java/com/management_system/utilities/web/WebConfig.java
@@ -6,11 +6,13 @@ import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
